885d49192c44c701b1d83d73960cde7d42c1b1ae
vyonna <vyonna.njenga@student.moringaschool.com>
2022-04-16 19:28:52 +0300

    updated installed apps

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tinymce',
+    'hood',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
