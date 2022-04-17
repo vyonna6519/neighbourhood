ac85172b60f63af5d0db72e3ad3f6fbac91ba4f2
vyonna <vyonna.njenga@student.moringaschool.com>
2022-04-17 20:16:18 +0300

    added tests

--- hood/tests.py
+++ hood/tests.py
@@ -1,24 +1,24 @@
 from django.test import TestCase
-from .models import neighbourhood,healthservices
+from .models import neighbourhood
 from django.contrib.auth.models import User
 import datetime as dt
 # Create your tests here.
 class neighbourhoodTestClass(TestCase):
     def setUp(self):
-        self.Ruaka = neighbourhood(neighbourhood='Ruaka')
+        self.Thika = neighbourhood(neighbourhood='Thika')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.Ruaka,neighbourhood))
+        self.assertTrue(isinstance(self.Thika,neighbourhood))
 
     def tearDown(self):
         neighbourhood.objects.all().delete()
 
     def test_save_method(self):
-        self.Ruaka.save_neighbourhood()
+        self.Thika.save_neighbourhood()
         hood = neighbourhood.objects.all()
         self.assertTrue(len(hood)>0)
 
     def test_delete_method(self):
-        self.Ruaka.delete_neighbourhood('Ruaka')
+        self.Thika.delete_neighbourhood('Thika')
         hood = neighbourhood.objects.all()
         self.assertTrue(len(hood)==0)
