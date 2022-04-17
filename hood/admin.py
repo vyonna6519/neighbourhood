5e51604650d9feb1465a8547e970f7200630a3f1
vyonna <vyonna.njenga@student.moringaschool.com>
2022-04-17 20:15:27 +0300

    registered models

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import neighbourhood,BlogPost,Profile
+
+
+class HealthAdmin(admin.ModelAdmin):
+    filter_horizontal =['healthservices']
 
 # Register your models here.
+admin.site.register(neighbourhood)
+admin.site.register(BlogPost)
+admin.site.register(Profile)
+
+
