9feee12bcc2482a385d2adbba50eb1986141a066
vyonna <vyonna.njenga@student.moringaschool.com>
2022-04-17 20:16:56 +0300

    added urls

--- hood/urls.py
+++ hood/urls.py
@@ -1,20 +1,26 @@
-from django.urls import path,include
+from django.urls import re_path,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns=[
-    path('',views.index,name='Index'),
-    path('blog',views.blog, name='blog'),
-    path('view/blog/(\d+)',views.view_blog,name='view_blog'),
-    path('my-profile/',views.my_profile, name='my-profile'),
-    # path('user/(<username>\)',views.user_profile,name='user-profile'),
-    path('new/blogpost',views.new_blogpost, name='new-blogpost'),
-    path('create/profile',views.create_profile, name='create-profile'),
-    path('update/profile',views.update_profile, name='update-profile'),
-    path('search/',views.search_results, name='search_results'),
+    re_path(r'^$',views.index,name='Index'),
+    re_path(r'^blog',views.blog, name='blog'),
+    re_path(r'^view/blog/(\d+)',views.view_blog,name='view_blog'),
+    re_path(r'^my-profile/',views.my_profile, name='my-profile'),
+    re_path(r'^user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
+    re_path(r'^new/blogpost$',views.new_blogpost, name='new-blogpost'),
+    re_path(r'^create/profile$',views.create_profile, name='create-profile'),
+    re_path(r'^update/profile$',views.update_profile, name='update-profile'),
+    re_path(r'^search/',views.search_results, name='search_results'),
+    re_path(r'accounts/',include('django_registration.backends.one_step.urls')),
+    re_path(r'accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^login/', auth_views.LoginView.as_view(), {"next_page": '/'}),
 
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
